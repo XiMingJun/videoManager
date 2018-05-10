7224c5dbb716926d8cff6ad1de15ea2b9f4b67eb
wangjian <wangjian@qhfax.com.cn>
2018-05-10 11:33:25 +0800

    提交sql文件

---
+++ videomanager.sql
@@ -0,0 +1,85 @@
+CREATE DATABASE 'videomanager';
+
+USE 'videomanager';
+
+-- 管理员表
+CREATE TABLE `videomanager`.`user` IF NOT EXISTS  (
+  `user_id` bigint(10) NOT NULL COMMENT '用户id',
+  `name` varchar(20) NOT NULL COMMENT '用户名',
+  `password` varchar(20) NOT NULL COMMENT '密码',
+	`gender` tinyint(1) NULL DEFAULT 1 COMMENT '性别 0 女，1 男',
+	`portrait` varchar(30)  COMMENT '头像',
+  `birthday` datetime(0) NULL  COMMENT '出生日期',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除 0 未删除，1 已删除',
+  `gmt_create` datetime(0) NULL  COMMENT '注册时间',
+  `gmt_modified` datetime(0) NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '上次更新时间',
+  PRIMARY KEY (`user_id`)
+);
+
+-- 管理员表
+CREATE TABLE `videomanager`.`admin_user` IF NOT EXISTS(
+  `admin_id` bigint(10) NOT NULL COMMENT '管理员id',
+  `name` varchar(20) NOT NULL COMMENT '用户名',
+  `password` varchar(20) NOT NULL COMMENT '密码',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除 0 未删除，1 已删除',
+  `gmt_create` datetime(0) NULL  COMMENT '账号产生时间',
+  `gmt_modified` datetime(0) NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '上次更新时间',
+  PRIMARY KEY (`admin_id`)
+);
+
+-- 视频表
+CREATE TABLE `videomanager`.`video` IF NOT EXISTS (
+  `video_id` bigint(10) NOT NULL COMMENT '视频id',
+  `course_id` bigint(10) NULL COMMENT '课程id',
+  `teacher_id` bigint(10) NULL COMMENT '讲师id',
+  `title` varchar(30) NULL COMMENT '标题',
+  `video_length` int(20) NULL COMMENT '视频长度',
+  `cover` varchar(255) NULL COMMENT '视频封面',
+  `video_address` varchar(255) NULL COMMENT '视频地址',
+  `description` varchar(255) NULL COMMENT '视频简介',
+  `gmt_create` datetime(0) NULL COMMENT '视频新增时间',
+  `create_userId` bigint(10) NULL COMMENT '操作管理员用户',
+  `gmt_update` datetime(0) NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+	`is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除 0 未删除，1 已删除'
+  PRIMARY KEY (`video_id`)
+);
+
+-- 讲师表
+CREATE TABLE `videomanager`.`teacher` IF NOT EXISTS  (
+  `teacher_id` bigint(10) NOT NULL COMMENT '讲师id',
+  `name` varchar(20) NOT NULL COMMENT '讲师姓名',
+  `gender` tinyint(1) NULL DEFAULT 1 COMMENT '性别 0 女，1 男',
+  `gmt_modified` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '上次更新时间',
+  `portrait` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除 0 未删除，1 已删除',
+  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `description` varchar(255) NULL COMMENT '简介',
+  `job` varchar(50) NULL COMMENT '职务',
+  `create_admin` bigint(10) NULL COMMENT '创建操作员',
+  `modified_admin` bigint(10) NULL COMMENT '修改操作员',
+  PRIMARY KEY (`teacher_id`)
+);
+
+-- 课程表
+CREATE TABLE `videomanager`.`course` IF NOT EXISTS (
+  `course_id` bigint(10) NOT NULL COMMENT '课程id',
+  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `gmt_modified` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '上次更新时间',
+  `create_admin` bigint(10) NULL DEFAULT NULL COMMENT '创建操作员',
+  `modified_admin` bigint(10) NULL DEFAULT NULL COMMENT '修改操作员',
+  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '简介',
+  `subject_id` bigint(10) NULL COMMENT '学科id',
+  PRIMARY KEY (`course_id`)
+);
+
+-- 学科表
+CREATE TABLE `videomanager`.`course` IF NOT EXISTS (
+  `subject_id` bigint(10) NOT NULL COMMENT '学科id',
+  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `gmt_modified` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '上次更新时间',
+  `create_admin` bigint(10) NULL DEFAULT NULL COMMENT '创建操作员',
+  `modified_admin` bigint(10) NULL DEFAULT NULL COMMENT '修改操作员',
+  `title` varchar(50) NULL COMMENT '学科名称',
+  PRIMARY KEY (`subject_id`)
+);
